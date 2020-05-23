043cb3c5c7fa707d6adb3d89cbfa23d64d894c13
2303386665@qq.com <2303386665@qq.com>
2020-05-23 20:04:08 +0800

    controller

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers\Test;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
     	xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
 
-        // 检测一个数据库的操作
-        new               
+        $user_id = empty($request->input('user_id'))?100002:$request->input('user_id');
 
+        // 检测一个数据库的操作       
+        $user = DB::select('select * from es_user where user_id = ?', [$user_id]);
 
 		$info = xhprof_disable();
         print_r($info);
+        // print_r($user);
     }
 }
